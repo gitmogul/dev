22ac14afe0e8d4f31ce49cb74eb9f12f5930b9bf
Shenoy Gurudutt <shenoyg@drona.localdomain>
2018-08-05 11:38:34 -0700

    added hello_world_module_param

--- linux/kmod/hello_world_module_param/hello_world.c
+++ linux/kmod/hello_world_module_param/hello_world.c
@@ -1,10 +1,18 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
+// Comments:
+// sudo insmod hello_world.ko PRINT_COUNT=5
+// This will result in the hello world greeting printed 5 times
 //
+// To see the value of PRINT_COUNT after the driver is inserted
+// sudo cat /sys/modules/hello_world/parameters/PRINT_COUNT
+// To change it after the module is inserted...
+// echo -n "10" > /sys.modules/hello_word/parameters/PRINT_COUNT 
 //
 ///////////////////////////////////////////////////////////////////////////////
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -12,14 +20,19 @@
 #define DESCRIPTION "Hello world example"
 #define VERSION     "1.0"
 
+int PRINT_COUNT = 1;
+module_param( PRINT_COUNT, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
  
 //////////////////////////////////////////////////////////////////////////////
 static int __init init_hello_world( void )
 {
     int ret = 0;
+    int count = 0;
 
     pr_info( "%s\n", __FUNCTION__ );
-    pr_info ("KE PASA MANG !!!\n" );
+    for( count = 0; count < PRINT_COUNT; count++ ) {
+        pr_info( "%s\n", "KE PASA MANG !!!" );
+    }
  
     return ret;
 }
@@ -28,8 +41,12 @@ static int __init init_hello_world( void )
 //////////////////////////////////////////////////////////////////////////////
 static void __exit exit_hello_world( void )
 {
+    int count;
+
     pr_info( "%s\n", __FUNCTION__ );
-    pr_info ("ADIOS MANG !!!\n" );
+    for( count = 0; count < PRINT_COUNT; count++ ) {
+        pr_info( "ADIOS MANG !!!\n" );
+    }
     return;
 }
 
